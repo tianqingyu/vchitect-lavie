bd2f94d5ac58151ed0f50e0365131fbc81df5e54
ruohe <god.yangjun@163.com>
2023-12-08 01:20:07 +0800

    2

--- demo/sample.py
+++ demo/sample.py
@@ -1,15 +1,13 @@
+import os
 import torch
-from torch import autocast
 from diffusers import StableDiffusionPipeline
 from PIL import Image
-import os
 
 pipe = StableDiffusionPipeline.from_pretrained("../pretrained_models/stable-diffusion-v1-4", torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
-with autocast("cuda"):
-    image = pipe(prompt).images[0]
+image = pipe(prompt).images[0]
 
 save_path = "../results/demo/1.png"
 os.makedirs(os.path.dirname(save_path), exist_ok=True)
