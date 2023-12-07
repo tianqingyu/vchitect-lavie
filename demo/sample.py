6b8944f9be39ac4182bbeef093990d7bc2821143
ruohe <god.yangjun@163.com>
2023-12-08 01:09:19 +0800

    1

--- demo/sample.py
+++ demo/sample.py
@@ -1,6 +1,7 @@
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 from PIL import Image
+import os
 
 pipe = StableDiffusionPipeline.from_pretrained("../pretrained_models/stable-diffusion-v1-4", use_auth_token=True)
 pipe = pipe.to("cuda")
@@ -10,5 +11,6 @@ with autocast("cuda"):
     image = pipe(prompt).images[0]
 
 save_path = "../results/demo/1.png"
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
 image.save(save_path)
 print(f"Image saved to {save_path}")
