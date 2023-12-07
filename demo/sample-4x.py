fecce0ca1a00b71c12af0cf7f8bb054b12fb97f4
ruohe <god.yangjun@163.com>
2023-12-08 02:19:40 +0800

    1

--- demo/sample-4x.py
+++ demo/sample-4x.py
@@ -2,8 +2,8 @@ import os
 import torch
 from diffusers import StableDiffusionPipeline
 from PIL import Image
-import requests
-from io import BytesIO
+from einops import rearrange
+import torchvision
 
 print('start')
 
@@ -15,12 +15,26 @@ print('load model')
 
 # load image
 input_path = "../results/demo/1.png"
-low_res_img = Image.open(input_path).convert("RGB")
+vframes, aframes, info = torchvision.io.read_video(filename=input_path, pts_unit='sec', output_format='TCHW') # RGB
+vframes = vframes / 255.
+vframes = (vframes - 0.5) * 2 # T C H W [-1, 1]
+t, _, h, w = vframes.shape
+vframes = vframes.unsqueeze(dim=0) # 1 T C H W
+vframes = rearrange(vframes, 'b t c h w -> b c t h w').contiguous()  # 1 C T H W
+print('Input_shape:', vframes.shape)
 print('load image')
 
 # gen
 prompt = "a photo of an astronaut riding a horse on mars"
-up_image = pipe(prompt=prompt, image=[low_res_img]).images[0]
+generator = torch.Generator(device='cuda').manual_seed(10)
+up_image = pipe(
+  prompt,
+  image=vframes,
+  generator=generator,
+  num_inference_steps=50,
+  guidance_scale=5,
+  noise_level=50
+).images[0]
 print('upscale done!')
 
 # output
