581696c7015cf255baef1283d9b7d79179c17953
ruohe <god.yangjun@163.com>
2023-12-08 02:31:12 +0800

    3

--- demo/sample-4x.py
+++ demo/sample-4x.py
@@ -19,10 +19,7 @@ print('load image')
 
 # gen
 up_image = pipe(prompt="a photo of an astronaut riding a horse on mars",
-                image=low_image,
-                num_inference_steps=50,
-                guidance_scale=5,
-                noise_level=50).images[0]
+                image=low_image).images[0]
 print('upscale done!')
 
 # output
