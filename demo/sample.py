2c2a013a9afd221efe1d4ace92e2a0428190b982
ruohe <god.yangjun@163.com>
2023-12-08 01:04:01 +0800

    1

---
+++ demo/sample.py
@@ -0,0 +1,14 @@
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from PIL import Image
+
+pipe = StableDiffusionPipeline.from_pretrained("../pretrained_models/stable-diffusion-v1-4", use_auth_token=True)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+with autocast("cuda"):
+    image = pipe(prompt).images[0]
+
+save_path = "../results/demo/1.png"
+image.save(save_path)
+print(f"Image saved to {save_path}")
