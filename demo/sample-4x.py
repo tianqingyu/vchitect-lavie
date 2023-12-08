67974f2d37925d8673541a3e9cccb04e9ed5cb23
ruohe <god.yangjun@163.com>
2023-12-08 12:44:19 +0800

    1

--- demo/sample-4x.py
+++ demo/sample-4x.py
@@ -4,18 +4,28 @@ from diffusers import StableDiffusionUpscalePipeline
 from PIL import Image
 import concurrent.futures
 
-def upscale_image(img, idx, pipe):
-    print(f'Start processing image {idx}')
-    up_image = pipe(
-        prompt='a photo of an astronaut riding a horse on mars',
-        image=img,
-        num_inference_steps=50,
-        guidance_scale=5,
-        noise_level=50
-    ).images[0]
+def upscale_image(img, idx, pipe, gpu_id):
+    device = f'cuda:{gpu_id}'
+    print(f'Start processing image {idx} on {device}')
+    
+    # 将模型移动到指定的 GPU
+    pipe = pipe.to(device)
+    pipe.enable_attention_slicing()
+    pipe.set_use_memory_efficient_attention_xformers(True)
+
+    # 处理图像
+    with torch.cuda.device(gpu_id):
+        up_image = pipe(
+            prompt='a photo of an astronaut riding a horse on mars',
+            image=img,
+            num_inference_steps=50,
+            guidance_scale=5,
+            noise_level=50
+        ).images[0]
+
     print(f'Upscale done! img = {idx}')
 
-    # output
+    # 输出路径
     output_path = f'../results/demo/{idx}-4x.png'
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     up_image.save(output_path)
@@ -24,20 +34,18 @@ def upscale_image(img, idx, pipe):
 def main():
     print('start')
 
-    # load model and scheduler
+    # 加载模型
     pipe = StableDiffusionUpscalePipeline.from_pretrained("../pretrained_models/stable-diffusion-x4-upscaler", torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
-    pipe.enable_attention_slicing()
-    pipe.set_use_memory_efficient_attention_xformers(True)
     print('load model')
 
-    # load image
+    # 加载图像
     low_images = [Image.open(f'../results/demo/{i}.png').convert('RGB') for i in range(1, 4)]
     print('load image')
 
-    # Use ThreadPoolExecutor for parallel processing
-    with concurrent.futures.ThreadPoolExecutor(max_workers=torch.cuda.device_count()) as executor:
-        futures = [executor.submit(upscale_image, img, i, pipe) for i, img in enumerate(low_images)]
+    # 使用 ThreadPoolExecutor 进行并行处理
+    num_gpus = torch.cuda.device_count()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_gpus) as executor:
+        futures = [executor.submit(upscale_image, img, idx, pipe, idx % num_gpus) for idx, img in enumerate(low_images)]
         concurrent.futures.wait(futures)
 
 if __name__ == "__main__":
