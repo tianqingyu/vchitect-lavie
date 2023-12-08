06633cf0d530b3c62d9a196a2fb5123441519df1
ruohe <god.yangjun@163.com>
2023-12-08 12:49:40 +0800

    2

--- demo/sample-4x.py
+++ demo/sample-4x.py
@@ -39,7 +39,7 @@ def main():
     print('load model')
 
     # 加载图像
-    low_images = [Image.open(f'../results/demo/{i}.png').convert('RGB') for i in range(1, 1)]
+    low_images = [Image.open(f'../results/demo/1.png').convert('RGB')]
     print('load image')
 
     # 使用 ThreadPoolExecutor 进行并行处理
